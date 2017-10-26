1d128cef70de415dcedb59c46f985f3ec3b1ef77
zhenghe <417296832@qq.com>
2017-10-26 15:18:44 +0800

    修正BUG

--- fangjia/spiders/fangjia.py
+++ fangjia/spiders/fangjia.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 import scrapy
-from fangjia.items import FangjiaItem
+from ..items import FangjiaItem
+from numpy import *
+import pdb
 
-headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'}
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
 
 class fangjiaSpider(scrapy.Spider):
     name = "fangjia"
@@ -16,7 +18,6 @@ class fangjiaSpider(scrapy.Spider):
             url = urlhead+'pg%snht1' % i
             self.start_urls.append(url)
         for url in self.start_urls:
-            print (url)
             yield scrapy.Request(url, headers=headers, callback=self.parse)
 
     def parse(self, response):
@@ -29,17 +30,17 @@ class fangjiaSpider(scrapy.Spider):
 
     def parse_fangjia(self, response):   # /是在根节点找(只找根节点下面一层,绝对) //是在根节点下面的所有节点找,相对
         item = FangjiaItem()
-        name = response.xpath('//div[@class="name-box"]/a/@title').extract()[0]
+        title = response.xpath('//div[@class="name-box"]/a/@title').extract()[0]
         url = response.xpath('//div[@class="name-box"]/a/@href').extract()[0]
         price = response.xpath('//p[@class="jiage"]/span[@class="junjia"]/text()').extract()[0]
-        address = response.xpath('//p[@class="where"]/span/@title').extract()[0]
-        item['FANGJIA_NAME'] = name
+        address = response.xpath('//div[@class="bottom-info"]/p[@class="where "]/span/@title').extract()[0]
+        item['FANGJIA_NAME'] = title
         item['FANGJIA_ADDRESS'] = address
         item['FANGJIA_PRICE'] = price
         item['FANGJIA_URL'] = 'http://cd.fang.lianjia.com'+url
-        print (item['FANGJIA_NAME'])
-        print (item['FANGJIA_ADDRESS'])
-        print (item['FANGJIA_PRICE'])
-        print (item['FANGJIA_URL'])
+        # print (item['FANGJIA_NAME'])
+        # print (item['FANGJIA_ADDRESS'])
+        # print (item['FANGJIA_PRICE'])
+        # print (item['FANGJIA_URL'])
         yield item
 
